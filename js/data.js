9302f34cf258f068ec80f7d0dbb3998456af9054
Eirik Vullum <evullum@gmail.com>
2014-12-07 23:51:56 +0100

    initial commit

---
+++ js/data.js
@@ -0,0 +1,487 @@
+var fbData = {
+    "data": [{
+        "location": "Rovika",
+        "name": "Treidagsvorsh",
+        "start_time": "2014-12-27",
+        "id": "617881824989126",
+        "rsvp_status": "attending"
+    }, {
+        "location": "Den Gamle Major",
+        "name": "30års lag",
+        "start_time": "2014-11-22T19:00:00+0100",
+        "timezone": "Europe/Oslo",
+        "id": "943336202346618",
+        "rsvp_status": "attending"
+    }, {
+        "name": "Bursdagsfeiring!!!!",
+        "start_time": "2014-11-08",
+        "id": "760665964008855",
+        "rsvp_status": "attending"
+    }, {
+        "location": "SBoS og omegn ",
+        "name": "Tony & Linda feirer seg selv... Anno 2014.",
+        "start_time": "2014-09-27T17:00:00+0200",
+        "timezone": "Europe/Oslo",
+        "id": "262617527259137",
+        "rsvp_status": "attending"
+    }, {
+        "location": "Observatoriegata 10",
+        "name": "Gebursdag - YOLO",
+        "start_time": "2014-09-06T18:00:00+0200",
+        "timezone": "Europe/Oslo",
+        "id": "264111690452579",
+        "rsvp_status": "attending"
+    }, {
+        "location": "Bislett stadion",
+        "name": "FEELINGS FESTIVAL - 22. AUGUST 2014",
+        "start_time": "2014-08-22T12:00:00+0200",
+        "timezone": "Europe/Oslo",
+        "id": "619095084826790",
+        "rsvp_status": "attending"
+    }, {
+        "location": "Hovefestivalen",
+        "name": "HOVEFESTIVALEN 2014",
+        "start_time": "2014-06-28",
+        "id": "471622502954965",
+        "rsvp_status": "attending"
+    }, {
+        "location": "rovika",
+        "name": "Vorsh",
+        "start_time": "2014-06-14T13:00:00+0200",
+        "timezone": "Europe/Oslo",
+        "id": "239516616246199",
+        "rsvp_status": "attending"
+    }, {
+        "location": "Oslo",
+        "name": "Bursdagsishfest",
+        "start_time": "2014-03-29T18:30:00+0100",
+        "timezone": "Europe/Oslo",
+        "id": "290546694434087",
+        "rsvp_status": "attending"
+    }, {
+        "location": "Rosenborggata 1A",
+        "name": "Bursdags- og velkommen til Oslo-vors",
+        "start_time": "2014-03-22T19:30:00+0100",
+        "timezone": "Europe/Oslo",
+        "id": "296627973824251",
+        "rsvp_status": "attending"
+    }, {
+        "location": "Trysil Fageråsen",
+        "name": "Tupptur til Trysil",
+        "start_time": "2014-02-19",
+        "timezone": "Europe/Oslo",
+        "id": "624701130919817",
+        "rsvp_status": "attending"
+    }, {
+        "location": "Nordbyveien 30, 2013 Skjetten",
+        "name": "Innflytningsfest",
+        "start_time": "2014-02-01T19:00:00+0100",
+        "timezone": "Europe/Oslo",
+        "id": "602443093142181",
+        "rsvp_status": "attending"
+    }, {
+        "location": "Rovika",
+        "name": "Treidagsvorsh",
+        "start_time": "2013-12-27",
+        "timezone": "Europe/Oslo",
+        "id": "605384416175594",
+        "rsvp_status": "attending"
+    }, {
+        "location": "Sannergata Bo- og Servicesenter",
+        "name": "Førjulsstemning",
+        "start_time": "2013-12-13T19:00:00+0100",
+        "timezone": "Europe/Oslo",
+        "id": "392729360857841",
+        "rsvp_status": "attending"
+    }, {
+        "location": "Markveien 34",
+        "name": "PARTY",
+        "start_time": "2013-12-06T20:00:00+0100",
+        "timezone": "Europe/Oslo",
+        "id": "1426169714266402",
+        "rsvp_status": "attending"
+    }, {
+        "location": "Teltet, Dødens Dal",
+        "name": "Calvin Harris (UK) + CLMD til UKA [UTSOLGT]",
+        "start_time": "2013-10-17T18:30:00+0200",
+        "timezone": "Europe/Oslo",
+        "id": "480548332042149",
+        "rsvp_status": "attending"
+    }, {
+        "location": "Sannergata Bo- og Servicesenter og omegn",
+        "name": "Tony & Linda feirer seg selv... The Reunion",
+        "start_time": "2013-09-28T17:00:00+0200",
+        "id": "556638347707096",
+        "rsvp_status": "attending"
+    }, {
+        "location": "Øratorget",
+        "name": "DDE",
+        "start_time": "2013-06-21T22:00:00+0200",
+        "timezone": "Europe/Oslo",
+        "id": "128916960644766",
+        "rsvp_status": "attending"
+    }, {
+        "location": "Sannergata Bo- og servicesenter",
+        "name": "17.mai frokost",
+        "start_time": "2013-05-17T09:30:00+0200",
+        "timezone": "Europe/Oslo",
+        "id": "154568421377869",
+        "rsvp_status": "attending"
+    }, {
+        "location": "Sannergata Bo- og Servicesenter",
+        "name": "Farvel O'store Tørketid",
+        "start_time": "2013-05-03T19:00:00+0200",
+        "timezone": "Europe/Oslo",
+        "id": "602420459777854",
+        "rsvp_status": "attending"
+    }, {
+        "location": "Sannergata bo- og servicesenter",
+        "name": "Etterjulsstemning: Reborn",
+        "start_time": "2013-01-05T19:00:00+0100",
+        "timezone": "Europe/Oslo",
+        "id": "478731708844685",
+        "rsvp_status": "attending"
+    }, {
+        "location": "Leira Forsamlingshus",
+        "name": "5 dags fæst",
+        "start_time": "2012-12-28",
+        "id": "446019545459547",
+        "rsvp_status": "attending"
+    }, {
+        "location": "Rovika",
+        "name": "Vors 3. juledag",
+        "start_time": "2012-12-27T18:00:00+0100",
+        "timezone": "Europe/Oslo",
+        "id": "576200992396816",
+        "rsvp_status": "attending"
+    }, {
+        "location": "Sannergata bo- og servicesenter",
+        "name": "Frædagskaffi med noko attåt",
+        "start_time": "2012-11-23T19:00:00+0100",
+        "timezone": "Europe/Oslo",
+        "id": "496119097087490",
+        "rsvp_status": "attending"
+    }, {
+        "location": "Niels Juels gate 43A",
+        "name": "Halloween Fest!",
+        "start_time": "2012-11-03T19:30:00+0100",
+        "timezone": "Europe/Oslo",
+        "id": "186499811486489",
+        "rsvp_status": "attending"
+    }, {
+        "location": "TBA",
+        "name": "Bagler'n mini reunion!",
+        "start_time": "2012-10-12",
+        "id": "454327067923472",
+        "rsvp_status": "attending"
+    }, {
+        "location": "Sør Tustna Forsamlingshus",
+        "name": "Giftermål på Tustna",
+        "start_time": "2012-07-14T20:00:00+0200",
+        "timezone": "Europe/Oslo",
+        "id": "167118843415367",
+        "rsvp_status": "attending"
+    }, {
+        "location": "Hjæm te mæ! ",
+        "name": "Drinky drinks evening!",
+        "start_time": "2012-05-18T18:00:00+0200",
+        "timezone": "Europe/Oslo",
+        "id": "222339551209743",
+        "rsvp_status": "attending"
+    }, {
+        "location": "Sannergata",
+        "name": "17.mai frokost",
+        "start_time": "2012-05-17T09:00:00+0200",
+        "timezone": "Europe/Oslo",
+        "id": "421861981176861",
+        "rsvp_status": "attending"
+    }, {
+        "end_time": "2012-03-18T20:00:00",
+        "location": "Trysilfjellet",
+        "name": "Tupphelg på Trysil",
+        "start_time": "2012-03-16T14:00:00",
+        "id": "275232259185843",
+        "rsvp_status": "attending"
+    }, {
+        "end_time": "2011-12-30T01:00:00",
+        "location": "Leira På Nordmore, More Og Romsdal",
+        "name": "5 - Juledagsfest i Leira forsamlingshus",
+        "start_time": "2011-12-29T21:00:00",
+        "id": "267638489957391",
+        "rsvp_status": "attending"
+    }, {
+        "end_time": "2011-12-27T20:00:00",
+        "location": "Rovika",
+        "name": "Tradisjonen fortsetter",
+        "start_time": "2011-12-27T17:00:00",
+        "id": "264952150227188",
+        "rsvp_status": "attending"
+    }, {
+        "end_time": "2011-10-30T03:00:00",
+        "name": "Kjøp/Salg av billetter for UKEfunker",
+        "start_time": "2011-10-30T00:00:00",
+        "id": "142237372541717",
+        "rsvp_status": "attending"
+    }, {
+        "end_time": "2011-11-12T23:00:00",
+        "location": "Studentersamfundet i Trondhjem",
+        "name": "Mistet/Funnet Samfundet UKA-11",
+        "start_time": "2011-10-24T17:30:00",
+        "id": "300875969922673",
+        "rsvp_status": "attending"
+    }, {
+        "end_time": "2011-10-21T20:30:00",
+        "location": "Dødens Dal",
+        "name": "Trønderfest m/DDE, Dag Ingebritsen & The Dynamite Kids og BCG UKA-11",
+        "start_time": "2011-10-21T17:30:00",
+        "id": "215909605136127",
+        "rsvp_status": "attending"
+    }, {
+        "end_time": "2011-10-21T09:00:00",
+        "location": "Studentersamfundet i Trondhjem, hele huset",
+        "name": "UKAs offisielle afterparty etter David Guetta UKA-11",
+        "start_time": "2011-10-20T19:00:00",
+        "id": "281647051845436",
+        "rsvp_status": "attending"
+    }, {
+        "end_time": "2011-10-07T20:30:00",
+        "location": "UKEbutikken",
+        "name": "Åpningsfest for UKEbutikken :-)",
+        "start_time": "2011-10-07T17:30:00",
+        "id": "115341231906416",
+        "rsvp_status": "attending"
+    }, {
+        "end_time": "2011-09-26T00:00:00",
+        "location": "Rica Hotell, Nidelven",
+        "name": "Maskeradeball for Øko, Salget og Kultur",
+        "start_time": "2011-09-25T18:30:00",
+        "id": "238077512890146",
+        "rsvp_status": "attending"
+    }, {
+        "end_time": "2011-09-25T20:30:00",
+        "location": "Øvre Møllenberg gate 66",
+        "name": "Billettvors før ballet",
+        "start_time": "2011-09-25T16:30:00",
+        "id": "176296985782258",
+        "rsvp_status": "attending"
+    }, {
+        "end_time": "2011-09-17T13:00:00",
+        "location": "Juvente",
+        "name": "Hyttetur!",
+        "start_time": "2011-09-15T12:00:00",
+        "id": "249984631705477",
+        "rsvp_status": "attending"
+    }, {
+        "end_time": "2011-09-14T20:00:00",
+        "location": "R1, Realfagbygget, Gløshaugen",
+        "name": "Kick-off for Salget",
+        "start_time": "2011-09-14T18:00:00",
+        "id": "107991155971385",
+        "rsvp_status": "attending"
+    }, {
+        "end_time": "2011-10-06T08:00:00",
+        "location": "Hybelen vår",
+        "name": "Åpningsfest for UKEbutikken",
+        "start_time": "2011-09-04T19:00:00",
+        "id": "267490429940516",
+        "rsvp_status": "attending"
+    }, {
+        "end_time": "2011-08-24T12:00:00",
+        "location": "Samfundet, Storsalen / NTNU Gløshaugen S2",
+        "name": "UKA-11: Infomøte om opptak!",
+        "start_time": "2011-08-23T15:30:00",
+        "id": "143208999099214",
+        "rsvp_status": "attending"
+    }, {
+        "end_time": "2011-04-21T02:00:00",
+        "location": "Sør Tustna Forsamlingshus",
+        "name": "Bluesaften m/ The Four Bluesbamd",
+        "start_time": "2011-04-20T21:30:00",
+        "id": "207578485922329",
+        "rsvp_status": "attending"
+    }, {
+        "end_time": "2011-04-20T22:30:00",
+        "location": "Amcar",
+        "name": "Vors",
+        "start_time": "2011-04-20T19:00:00",
+        "id": "149914068407287",
+        "rsvp_status": "attending"
+    }, {
+        "end_time": "2011-03-15T08:00:00",
+        "location": "Everywhere",
+        "name": "Coca-Cola: Involve yourself! TWO WEEKS FROM MARCH 1ST: JusticeforMartine!",
+        "start_time": "2011-03-01T08:00:00",
+        "id": "125781894159879",
+        "rsvp_status": "attending"
+    }, {
+        "end_time": "2011-02-26T23:30:00",
+        "location": "Korsgata 19c/b, 3. etasje",
+        "name": "SUPERheroPARTY",
+        "start_time": "2011-02-26T18:00:00",
+        "id": "175878599123078",
+        "rsvp_status": "attending"
+    }, {
+        "end_time": "2011-01-16T15:00:00",
+        "location": "Trysil",
+        "name": "Trysilhelg",
+        "start_time": "2011-01-14T16:00:00",
+        "id": "122403867822985",
+        "rsvp_status": "attending"
+    }, {
+        "end_time": "2010-12-28T02:00:00",
+        "location": "Sør Tustna Forsamlingshus",
+        "name": "Dansefest ",
+        "start_time": "2010-12-27T21:30:00",
+        "id": "168849473150293",
+        "rsvp_status": "attending"
+    }, {
+        "end_time": "2010-12-27T22:00:00",
+        "location": "Casa del Hagen",
+        "name": "3.dags vors!!",
+        "start_time": "2010-12-27T16:00:00",
+        "id": "178893202124948",
+        "rsvp_status": "attending"
+    }, {
+        "end_time": "2010-12-24T20:00:00",
+        "name": "Adventskalender ",
+        "start_time": "2010-12-01T10:00:00",
+        "id": "110888025648562",
+        "rsvp_status": "attending"
+    }, {
+        "end_time": "2010-11-13T06:00:00",
+        "location": "hæm åt åss",
+        "name": "Innflyttingsfest/bursdagsfest",
+        "start_time": "2010-11-12T18:00:00",
+        "id": "122144184508467",
+        "rsvp_status": "attending"
+    }, {
+        "end_time": "2010-10-31T05:00:00",
+        "location": "Hos Tom",
+        "name": "Halloween Party!",
+        "start_time": "2010-10-30T19:00:00",
+        "id": "156768631011755",
+        "rsvp_status": "attending"
+    }, {
+        "end_time": "2010-07-10T01:00:00",
+        "location": "Lofotgata, selvfølgelig",
+        "name": "<3",
+        "start_time": "2010-07-09T19:00:00",
+        "id": "129548120413816",
+        "rsvp_status": "attending"
+    }, {
+        "end_time": "2010-04-26T21:00:00",
+        "location": "Aulaen, Atlanten VGS",
+        "name": "Atlantens Russerevy 2010",
+        "start_time": "2010-04-26T19:00:00",
+        "id": "116689315023564",
+        "rsvp_status": "attending"
+    }, {
+        "end_time": "2009-12-18T14:00:00",
+        "location": "Same procedure as last year, miss Sophie",
+        "name": "Juleballvors",
+        "start_time": "2009-12-17T19:00:00",
+        "id": "195204909073",
+        "rsvp_status": "attending"
+    }, {
+        "end_time": "2009-10-16T23:00:00",
+        "location": "Dødens Dal",
+        "name": "Trønderfest m/Åge",
+        "start_time": "2009-10-16T17:00:00",
+        "id": "140069742679",
+        "rsvp_status": "unsure"
+    }, {
+        "end_time": "2009-09-19T01:00:00",
+        "location": "Lofotgata ",
+        "name": "Boller og brus",
+        "start_time": "2009-09-18T19:00:00",
+        "id": "112027088962",
+        "rsvp_status": "attending"
+    }, {
+        "end_time": "2009-06-07T01:00:00",
+        "location": "Nordvang Grendahus",
+        "name": "Tustnadag",
+        "start_time": "2009-06-06T13:00:00",
+        "id": "75755874270",
+        "rsvp_status": "attending"
+    }, {
+        "end_time": "2009-01-15T16:00:00",
+        "location": "Glassgården, Elektrobygget",
+        "name": "Elektronikk- & Telekommunikasjonsdagen 09",
+        "start_time": "2009-01-15T10:00:00",
+        "id": "53902621094",
+        "rsvp_status": "attending"
+    }, {
+        "end_time": "2008-10-26T01:00:00",
+        "location": "Norge ",
+        "name": "2nd Drill Reunion 2006",
+        "start_time": "2008-10-24T18:00:00",
+        "id": "25816758831",
+        "rsvp_status": "attending"
+    }, {
+        "end_time": "2008-10-18T06:00:00",
+        "location": "Klæbuveien 106",
+        "name": "Den STORE innflyttningsfesten",
+        "start_time": "2008-10-17T19:00:00",
+        "id": "33363377030",
+        "rsvp_status": "attending"
+    }, {
+        "end_time": "2008-09-27T20:00:00",
+        "location": "Nedre Møllenberggate 63b",
+        "name": "LAN@NedreMøllenbergGate63.com",
+        "start_time": "2008-09-26T20:00:00",
+        "id": "40024591787",
+        "rsvp_status": "attending"
+    }, {
+        "end_time": "2008-03-30T13:00:00",
+        "location": "Håkon's Crib",
+        "name": "Kalas på lørdan",
+        "start_time": "2008-03-29T03:00:00",
+        "id": "12080128083",
+        "rsvp_status": "attending"
+    }, {
+        "end_time": "2008-03-23T08:00:00",
+        "location": "Oppdal så klart",
+        "name": "Oppdal Påska 08",
+        "start_time": "2008-03-16T05:00:00",
+        "id": "6176643742",
+        "rsvp_status": "attending"
+    }, {
+        "end_time": "2007-12-27T23:00:00",
+        "location": "Rovika",
+        "name": "Tradisjonsvors",
+        "start_time": "2007-12-27T19:00:00",
+        "id": "7406128667",
+        "rsvp_status": "attending"
+    }, {
+        "end_time": "2007-12-28T13:00:00",
+        "location": "Sunndal. sted er ennå et problem",
+        "name": "Julereunion for avgangsklassen 2005 ved sunndal vgs!!",
+        "start_time": "2007-12-27T19:00:00",
+        "id": "6075478350",
+        "rsvp_status": "attending"
+    }, {
+        "end_time": "2007-12-13T12:00:00",
+        "location": "Kinosalen på hybelen til Vullum!",
+        "name": "Trondheimspremiære!",
+        "start_time": "2007-12-13T07:00:00",
+        "id": "7473607773",
+        "rsvp_status": "attending"
+    }],
+    "paging": {
+        "cursors": {
+            "before": "TmpFM09EZ3hPREkwT1RnNU1USTJPakUwTVRrMk5qY3lNREE2TVRZMU1EZzBPRGsyT0RRNE5UZ3g=",
+            "after": "TWpNeU9ESTVORGN4TVRveE1UZ3lNRFF5TURBd09qSTBNakU1TlRNek9UUT0="
+        },
+        "next": "https://graph.facebook.com/v2.2/10154623550215175/events?pretty=0&limit=5000&after=TWpNeU9ESTVORGN4TVRveE1UZ3lNRFF5TURBd09qSTBNakU1TlRNek9UUT0="
+    }
+};
+
+window.formatEventData = function(events) {
+  return events.map(function(event) {
+    return {
+      name: event.name + ' (' + (event.location ? event.location : 'Unknown') + ')',
+      start: event.start_time,
+      end: event.end_time
+    };
+  });
+}
---
+++ js/script.js
@@ -0,0 +1,115 @@
+var layOutTimeline = (function() {
+
+  "use strict";
+
+  function sortByProp(array, prop, desc) {
+    return array.slice().sort(function(a, b) {
+      return desc ? (a[prop] + b[prop]) : (a[prop] - b[prop]);
+    });
+  }
+
+  function generateArray(start, stop) {
+    var result = [];
+    var temp;
+    var ii;
+
+    if (stop < start) {
+      temp = stop;
+      stop = start;
+      start = temp;
+    }
+
+    for (ii = start; ii <= stop + 1; ii++) {
+      result.push(ii);
+    }
+
+    return result;
+  }
+
+  function generateTimeSpan(events) {
+    var min = events.map(function(event) {
+      return event.start.year();
+    }).reduce(function(a, b) {
+      return a < b ? a : b;
+    });
+
+    var max = events.map(function(event) {
+      return event.end ? event.end.year() : event.start.year();
+    }).reduce(function(a, b) {
+      return a > b ? a : b;
+    });
+
+    return generateArray(min, max);
+  }
+
+  function renderTimeSpanHTML(timeSpan, scale) {
+    return timeSpan.map(function(year, index) {
+      var style = [
+        'left: ' + ((365 * index) * scale) + 'px;',
+        'width: ' + (365 * scale) + 'px;',
+        'background-color: ' + (index % 2 ? '#080933' : 'none')
+      ].join('');
+
+      return [
+        '<div class="year" style="' + style + '">',
+        '<span>' + year + ' (' + (year - timeSpan[0]) + ')' + '</span>',
+        '</div>'
+      ].join('');
+    }).join('');
+  }
+
+  function renderTimeSpan(timeSpan, scale) {
+    var html = renderTimeSpanHTML(timeSpan, scale);
+    document.getElementById('timespan').innerHTML = html;
+    document.getElementById('timeline').style.width = (timeSpan.length * 365 * scale) + 1000 + 'px';
+  }
+
+  function renderEventsHTML(events, origin, scale) {
+    return events.map(function(event, index) {
+      var zeroDay = moment(new Date(origin,0,1));
+      var offset = moment(event.start).diff(zeroDay, 'days');
+      
+      var spanStyle = [
+        'float: left;',
+        'width:' + ((event.end.toString() != event.start.toString() ? event.end.diff(event.start, 'days')  * scale : 0)) + 'px;',
+      ].join('');
+      
+      var elementStyle = [
+        'margin-left: ' + ((offset) * scale) + 'px;'
+      ].join('');
+
+      return [
+        '<div class="event" style="' + elementStyle + '">',
+        '    <span class="timebar" style="' + spanStyle + '"></span>',
+        '    <p>' + event.name + '</p>',
+        '</div>'
+      ].join('')
+    }).join('');
+  }
+
+  function renderEvents(events, origin, scale) {
+    var html = renderEventsHTML(events, origin, scale);
+    document.getElementById('events').innerHTML = html;
+  }
+
+  function createDataArray(events) {
+    return events.map(function(event) {
+      if (event.end === '') event.end = new Date();
+
+      return {
+        name: event.name,
+        start: moment(event.start),
+        end: moment(event.end || event.start)
+      }
+    });
+  }
+
+  return function(events, scale) {
+    var scale = scale || 0.3;
+    var data = createDataArray(events);
+    var sortedData = sortByProp(data, 'start');
+    var timeSpan = generateTimeSpan(sortedData);
+    renderTimeSpan(timeSpan, scale);
+    renderEvents(sortedData, timeSpan[0], scale);
+  };
+}());
