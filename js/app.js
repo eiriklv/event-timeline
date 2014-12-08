242932d310677bd6f2094c654df0e1dde1388656
Eirik Vullum <evullum@gmail.com>
2014-12-08 01:23:18 +0100

    add better error handling

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@
 
         FB.login(function() {
             FB.api('/me/events?since=0&limit=5000', {}, function(response) {
-                if (response.data.length > 0) {
+                if (response.data && (response.data.length > 0)) {
                     layOutTimeline(response.data, 0.6);
                 } else {
                     alert('Sorry, you either have no events on facebook, or there\'s something wrong with the permissions');
